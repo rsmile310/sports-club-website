001a6d0d03e23b002e6678501042d8c86741872b
Daniel Ling <me@danielling.com>
2023-02-15 12:41:02 +0100

    First commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';1
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+
+// https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), preact()],
+});
