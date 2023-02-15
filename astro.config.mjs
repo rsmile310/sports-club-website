ecee8d964eab8b48b1cd452c166e34665384d04f
Daniel Ling <me@danielling.com>
2023-02-15 14:21:46 +0100

    additional data

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,5 +9,6 @@ import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [tailwind(), preact()],
 });
--- src/js/clubdata.js
+++ src/js/clubdata.js
@@ -1,10 +1,10 @@
 var sourceData = [
     {
-        id: "1",
+        id: 1,
         description: "Manchester United Football Club, commonly referred to as Man United (often stylised as Man Utd), or simply United, is a professional football club based in Old Trafford, Greater Manchester, England. The club competes in the Premier League, the top division in the English football league system.",
         name: "Manchester United",
         your_support: "$220",
-        all_support: "1624",
+        all_support: "1625",
         current: true,
         league: "Premier League, UK",
         image: "https://logos-world.net/wp-content/uploads/2020/06/Manchester-United-logo.png",
