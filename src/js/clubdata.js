1dd4e3c8ae4950f6d3aa9027ef52715145281881
Daniel Ling <me@danielling.com>
2023-02-15 14:43:14 +0100

    more clubdata example

--- src/js/clubdata.js
+++ src/js/clubdata.js
@@ -9,4 +9,14 @@ var sourceData = [
         league: "Premier League, UK",
         image: "https://logos-world.net/wp-content/uploads/2020/06/Manchester-United-logo.png",
     },
+    {
+        id: 2,
+        description: "Manchester United Football Club, commonly referred to as Man United (often stylised as Man Utd), or simply United, is a professional football club based in Old Trafford, Greater Manchester, England. The club competes in the Premier League, the top division in the English football league system.",
+        name: "Liverpool",
+        your_support: "$250",
+        all_support: "1625",
+        current: true,
+        league: "Premier League, UK",
+        image: "https://logos-world.net/wp-content/uploads/2020/06/Manchester-United-logo.png",
+    },
 ];
