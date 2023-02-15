c9ffbff80b8e687aed1bd282a1f15692862b6418
Daniel Ling <me@danielling.com>
2023-02-15 14:04:46 +0100

    Adjustments

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,8 @@ module.exports = {
 		},
 	},
 	plugins: [
-		require('@tailwindcss/forms')
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/aspect-ratio'),
 	]
 }
